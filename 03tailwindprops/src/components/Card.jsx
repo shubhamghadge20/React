38e95af9bd177d096874c397732177bb8ffa3272
Shubham Ghadge <shubhamghadge784@gmail.com>
2024-03-25 16:47:50 +0530

    basic of props and tailwind

---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+//Props is a special keyword in React that stands for properties and is used for passing data from one component to another.
+//below we get property/variable/props from app component
+//below username nad btntxt are props
+function Card({username , btnText}){
+
+    return(
+    <>
+    <div class="relative h-[400px] w-[300px] rounded-md">
+  <img
+    src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+    alt="AirMax Pro"
+    class="z-0 h-full w-full rounded-md object-cover"
+  />
+  <div class="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+  <div class="absolute bottom-4 left-4 text-left">
+    <h1 class="text-lg font-semibold text-white">{username}</h1>
+    <p class="mt-2 text-sm text-gray-300">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      debitis?
+    </p>
+    <button class="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white bg-black">
+    {btnText}
+    </button>
+  </div>
+</div>
+    </>
+    )
+}
+export default Card;
