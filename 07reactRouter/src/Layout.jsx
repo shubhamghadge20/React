6cc0a70e9cf6d0407e7fddc9eaa2cd575dede0cd
Shubham Ghadge <shubhamghadge784@gmail.com>
2024-07-19 21:26:41 +0530

    routers

---
+++ 07reactRouter/src/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
+
+function Layout() {
+  return (
+    <>
+    <Header/>
+    <Outlet />
+    <Footer />
+    </>
+  )
+}
+
+export default Layout
+
+//outlet use to change content between header and footer
